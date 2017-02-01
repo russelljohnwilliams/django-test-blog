bd850acbbd52008f6cf1464561a41a5884a0ed4a
Russell Williams <russellwilliamscc@gmail.com>
2017-02-01 16:05:35 +0000

    adds edit form and add form

--- blog/models.py
+++ blog/models.py
@@ -5,11 +5,12 @@ class Post(models.Model):
     author = models.ForeignKey('auth.User')
     title = models.CharField(max_length=200)
     text = models.TextField()
-    image = models.ImageField("Image", upload_to="blog/images/") 
+    image = models.ImageField(upload_to="static/blog/images/", blank=True) 
     created_date = models.DateTimeField(
             default=timezone.now)
     published_date = models.DateTimeField(
             blank=True, null=True)
+
 
     def publish(self):
         self.published_date = timezone.now()
--- mysite/settings.py
+++ mysite/settings.py
@@ -120,3 +120,8 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'blog')
+
+MEDIA_URL = '/blog/'
+
